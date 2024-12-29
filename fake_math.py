711f61ccf5d7437337312a3a751fbfac779292b7
LAVnuko <61196497+LAVnuko@users.noreply.github.com>
2024-12-29 19:34:47 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second > 0:
+        result_divide = first / second
+    elif second == 0:
+        result_divide = "Ошибка"
+    return result_divide
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    elif second > 0:
+        result_divide = first / second
+        return result_divide
+
